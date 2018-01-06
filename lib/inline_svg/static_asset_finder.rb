5c5161837e9d531efc59c797733c9178bcc5d25e
Tom Scott <tscott@weblinc.com>
2018-01-06 17:43:41 -0500

    Look for SVGs within engine asset paths
    In some situations within Rails 5.1, SVGs cannot be found when they are
    coming from within an engine and bundled into the Rails app. It's necessary
    to run `rails tmp:cache:clear assets:clobber assets:precompile` a second time
    in order to get them to resolve. If the initial compiled asset or
    manifest asset can not be found, we'll fall back to looking for the
    actual file bundled within a gem somewhere.

--- lib/inline_svg/static_asset_finder.rb
+++ lib/inline_svg/static_asset_finder.rb
@@ -13,15 +13,43 @@ module InlineSvg
     end
 
     def pathname
-      if ::Rails.application.config.assets.compile
-        ::Rails.application.assets[@filename].pathname
-      else
-        manifest = ::Rails.application.assets_manifest
-        asset_path = manifest.assets[@filename]
-        unless asset_path.nil?
-          ::Rails.root.join(manifest.directory, asset_path)
-        end
+      if previously_compiled?
+        compiled_pathname
+      elsif manifest_pathname.exist?
+        manifest_pathname
+      elsif engine_root.present?
+        engine_pathname
       end
+    end
+
+    private
+
+    def previously_compiled?
+      ::Rails.application.config.assets.compile && compiled_pathname.exist?
+    end
+
+    def compiled_pathname
+      ::Rails.application.assets[@filename].pathname
+    end
+
+    def manifest_pathname
+      manifest = ::Rails.application.assets_manifest
+      asset_path = manifest.assets[@filename]
+      unless asset_path.nil?
+        ::Rails.root.join(manifest.directory, asset_path)
+      end
+    end
+
+    def engine_pathname
+      engine_root.join(@filename)
+    end
+
+    def engine_root
+      engine_paths.find { |path| path.join(@filename).exist? }
+    end
+
+    def engine_paths
+      ::Rails.configuration.assets.paths.map { |path| Pathname.new(path) }
     end
   end
 end
